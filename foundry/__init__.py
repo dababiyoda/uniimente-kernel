8768e407a920a327f1d37e570076912c08e16420
Ultrainstictmasteryoda <128071340+dababiyoda@users.noreply.github.com>
2026-07-19 23:35:54 -0400

    Export Foundry underwriting wire parser

--- foundry/__init__.py
+++ foundry/__init__.py
@@ -1,4 +1,5 @@
 from .contracts import *
 from .engine import AdvantageFoundry
+from .wire import UNDERWRITING_SCHEMA_VERSION, opportunity_from_underwriting_wire
 
 __all__ = [name for name in globals() if not name.startswith("_")]

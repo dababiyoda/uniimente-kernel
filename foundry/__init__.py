5d57beba961b0c2cd2083a1d28da5466e8c21cb6
Ultrainstictmasteryoda <128071340+dababiyoda@users.noreply.github.com>
2026-07-19 23:41:25 -0400

    Export commercial closure compiler

--- foundry/__init__.py
+++ foundry/__init__.py
@@ -1,4 +1,5 @@
 from .contracts import *
+from .commercial import CommercialCase, CommercialClosureCompiler, CommercialStage, CommercialTransition
 from .engine import AdvantageFoundry
 from .wire import UNDERWRITING_SCHEMA_VERSION, opportunity_from_underwriting_wire
 
